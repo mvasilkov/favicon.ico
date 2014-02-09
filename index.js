af09c65d2e3867859ca833c90becd9bbf1b34cf8
Mark Vasilkov <mvasilkov@gmail.com>
2014-02-09 21:05:17 +0200

    RIVERS OF BLOOD ALL DAY EVERY DAY

---
+++ index.js
@@ -0,0 +1,86 @@
+var dns = require('dns'),
+    fs = require('fs'),
+    magick = require('imagemagick-native-2'),
+    request = require('request'),
+    firefox = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:28.0) '+
+              'Gecko/20100101 Firefox/28.0'
+
+function getMany(domains) {
+    if (domains.length) {
+        var domain = domains.pop().toLowerCase()
+        get(domain, function () { getMany(domains) })
+    }
+}
+
+function get(domain, done) {
+    if (resemblesDomainName(domain)) {
+        dns.resolve4(domain, function (err, addr) {
+            if (err) {
+                console.error('%s: cannot resolve (%s)', R(domain), err)
+                setImmediate(done)
+                return
+            }
+            get1(domain, done)
+        })
+    }
+    else {
+        console.error('%s: bad domain name', R(domain))
+        setImmediate(done)
+        return
+    }
+}
+
+function get1(domain, done) {
+    var url = 'http://' + domain + '/favicon.ico'
+    request(options(url), function (err, res, buf) {
+        res.statusCode == 200 || (err = err || res.statusCode)
+        var contentType = res.headers['content-type'] || 'n/a'
+        contentType.substr(0, 6) == 'image/' || (err = err || contentType)
+        if (err) {
+            console.error('%s: cannot download\n(%s)', R(url), res.request.href)
+            setImmediate(done)
+            return
+        }
+        save(buf, domain, done)
+    })
+}
+
+function save(buf, domain, done) {
+    var res
+    try {
+        res = magick.convert({
+            srcFormat: 'ICO',
+            srcData: buf,
+            format: 'PNG',
+            height: 16,
+            width: 16
+        })
+    }
+    catch (err) {
+        console.error('%s: cannot convert', R(domain))
+        setImmediate(done)
+        return
+    }
+    fs.writeFile(domain + '.png', res, function (err) {
+        if (err) console.error('%s: cannot save (%s)', R(domain), err)
+        setImmediate(done)
+    })
+}
+
+function resemblesDomainName(domain) {
+    return /^[a-z0-9-]+(\.[a-z0-9-]+)+$/.test(domain)
+}
+
+function R(str) { return '\u001b[91m' + str + '\u001b[0m' }
+
+function options(url) {
+    return {
+        url: url,
+        headers: {'user-agent': firefox},
+        jar: request.jar(),
+        timeout: 20000,
+        encoding: null
+    }
+}
+
+module.exports.getMany = getMany
