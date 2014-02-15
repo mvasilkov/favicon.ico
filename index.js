fa839498e75746cb9f460e928d35631633210a18
Mark Vasilkov <mvasilkov@gmail.com>
2014-02-15 22:25:15 +0200

    I HAVE NO TOOLS BECAUSE I’VE DESTROYED MY TOOLS WITH MY TOOLS
    0.0.2

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var dns = require('dns'),
     fs = require('fs'),
+    url = require('url'),
     magick = require('imagemagick-native-2'),
     request = require('request'),
+    cheerio = require('cheerio'),
     firefox = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:28.0) '+
               'Gecko/20100101 Firefox/28.0'
 
@@ -21,7 +23,7 @@ function get(domain, done) {
                 setImmediate(done)
                 return
             }
-            get1(domain, done)
+            get2(domain, done, function () { get1(domain, done) })
         })
     }
     else {
@@ -37,6 +39,7 @@ function get1(/* optional */ url, domain, done) {
         domain = url
         url = 'http://' + domain + '/favicon.ico'
     }
+    console.log('[1] =>', url)
     request(options(url), function (err, res, buf) {
         var err2, contentType
         if (err) {
@@ -54,6 +57,47 @@ function get1(/* optional */ url, domain, done) {
         }
         save(buf, domain, done)
     })
+}
+
+function get2(domain, done, fail) {
+    var url = 'http://' + domain
+    console.log('[2] =>', url)
+    request(options(url, 'utf8'), function (err, res, html) {
+        var err2, contentType
+        if (err) {
+            console.error('%s: cannot download\n(%s)', R(url), res.request.href)
+            setImmediate(fail)
+            return
+        }
+        contentType = res.headers['content-type'] || 'n/a'
+        contentType.substr(0, 5) == 'text/' || (err2 = contentType)
+        if (err2) {
+            console.error('%s: problem downloading (%s)', R(url), err2)
+            setImmediate(fail)
+            return
+        }
+        parse(html, res.request.href, domain, done, fail)
+    })
+}
+
+function parse(html, baseUrl, domain, done, fail) {
+    var $, link
+    try { $ = cheerio.load(html) }
+    catch (err) {
+        console.error('%s: cannot parse\n(%s)', R(domain), baseUrl)
+        fail()
+        return
+    }
+    link = $('link[rel=icon]').attr('href') ||
+           $('link[rel="shortcut icon"]').attr('href')
+    if (link) {
+        link = url.resolve(baseUrl, link)
+        get1(link, domain, function (saved) {
+            if (saved) done()
+            else fail()
+        })
+    }
+    else fail()
 }
 
 function save(buf, domain, done) {
@@ -74,7 +118,7 @@ function save(buf, domain, done) {
     }
     fs.writeFile(domain + '.png', res, function (err) {
         if (err) console.error('%s: cannot save (%s)', R(domain), err)
-        setImmediate(done)
+        setImmediate(function () { done(err === null) })
     })
 }
 
@@ -84,13 +128,13 @@ function resemblesDomainName(domain) {
 
 function R(str) { return '\u001b[91m' + str + '\u001b[0m' }
 
-function options(url) {
+function options(url, encoding) {
     return {
         url: url,
         headers: {'user-agent': firefox},
         jar: request.jar(),
         timeout: 20000,
-        encoding: null
+        encoding: encoding || null
     }
 }
 
